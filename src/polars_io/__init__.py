d2703436d1c85fd5368dce0f8925fafaed652c37
Alistair Pattison <alistairpattison@gmail.com>
2025-07-22 15:25:55 -0400

    add fwf to main export

--- src/polars_io/__init__.py
+++ src/polars_io/__init__.py
@@ -1,10 +1,13 @@
 from polars_io.stata import scan_stata, read_stata
 from polars_io.sas import scan_sas, read_sas
+from polars_io.fixed_width import scan_fwf, read_fwf
 
 
 __all__ = [
     "scan_stata",
     "scan_sas",
+    "scan_fwf",
     "read_stata",
     "read_sas",
+    "read_fwf",
 ]
